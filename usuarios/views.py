cb4c2bbbd4db57e11499d6aaf1368a2798dd608c
luanpoppe <luanpoppe@gmail.com>
2023-12-31 16:11:01 -0300

    Terminei a aula 03-05 - Alertas e mensagens

--- usuarios/views.py
+++ usuarios/views.py
@@ -4,6 +4,7 @@ from usuarios.forms import LoginForms, CadastroForms
 
 from django.contrib.auth.models import User
 from django.contrib import auth
+from django.contrib import messages
 
 def login(request):
     form = LoginForms()
@@ -22,8 +23,10 @@ def login(request):
         )
         if usuario is not None:
             auth.login(request, usuario)
+            messages.success(request, f"{nome} logado com sucesso!")
             return redirect("index")
         else:
+            messages.error(request, "Erro ao efetuar login")
             return redirect("login")
 
 
@@ -39,6 +42,7 @@ def cadastro(request):
 
         if form.is_valid():
             if form["senha_1"].value() != form["senha_2"].value():
+                messages.error(request, "Senhas não são iguais")
                 return redirect ('cadastro')
 
             nome=form['nome_cadastro'].value()
@@ -46,6 +50,7 @@ def cadastro(request):
             senha=form['senha_1'].value()
 
             if User.objects.filter(username=nome).exists():
+                messages.error(request, "Usuário já existente")
                 return redirect('cadastro')
 
             usuario = User.objects.create_user(
@@ -54,6 +59,7 @@ def cadastro(request):
                 password=senha
             )
             usuario.save()
+            messages.success(request, "Cadastro efetuado com sucesso")
             return redirect('login')
 
     return render(request, 'usuarios/cadastro.html', {'form': form})
